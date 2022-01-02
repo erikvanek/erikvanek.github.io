b9e966771826eb3f6871ed587bef1747f13957d0
Erik Vanek <vanek.erik@gmail.com>
2022-01-02 16:28:36 +0100

    slight redesign

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,16 @@
-// module.exports = {
-//     dir: {
-//       input: "pages",
-//       output: "docs"
-//     }
-//   };
-
-//   module.exports = function(eleventyConfig) {
-//     eleventyConfig.addPassthroughCopy('css')
-//     return {
-//       passthroughFileCopy: true
-//     }
-//   }
-
-  module.exports = function(eleventyConfig) {
-    return {
-      templateFormats: [
-        "md",
-        "jpg",
-        "mp4",
-        "pdf",
-        "ico",
-        "css" // css is not yet a recognized template extension in Eleventy
-      ],
-      dir: {
-        input: "pages",
-        output: "docs"
-      }
-    };
+module.exports = function (eleventyConfig) {
+  return {
+    templateFormats: [
+      "md",
+      "jpg",
+      "mp4",
+      "pdf",
+      "ico",
+      "css", // css is not yet a recognized template extension in Eleventy
+    ],
+    dir: {
+      input: "pages",
+      output: "docs",
+    },
   };
+};
