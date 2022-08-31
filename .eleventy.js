39f804d14bf630bea258f5b79242fd3b559a82f5
Erik Vanek <vanek.erik@gmail.com>
2022-08-31 16:28:40 +0200

    prezentace na trychtyr

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./pages')
   return {
     templateFormats: [
       "md",
@@ -6,6 +7,7 @@ module.exports = function (eleventyConfig) {
       "mp4",
       "pdf",
       "ico",
+      "html",
       "css", // css is not yet a recognized template extension in Eleventy
     ],
     dir: {
