9d8714daa2f273d4119216be3fe37bb868218bcf
Erik Vanek <vanek.erik@gmail.com>
2023-07-08 17:03:11 +0200

    nu WIP

--- docs/js/nu.js
+++ docs/js/nu.js
@@ -11,17 +11,20 @@ const blobs = [
     {
         adjective: 'inexhaustible',
         noun: 'city biker',
-        hint: 'not a biker but a commuter'
+        hint: 'not a biker but a commuter',
+        color: '#f77446',
     },
     {
         adjective: 'curious',
         noun: 'cook',
-        link: '/food -> page sourced from google photos'
+        link: '/food -> page sourced from google photos',
+        color: '#12c1dd'
     },
     {
         adjective: 'can do',
         noun: 'designer',
-        link: '/hire-me -> page about my thoughts + tykani'
+        link: '/hire-me -> page about my thoughts + tykani',
+        color: '#158f49'
     },
     {
         adjective: 'nerdy',
@@ -34,7 +37,7 @@ const blobs = [
     },
     {
         adjective: 'sensitive',
-        noun: 'explorer'
+        noun: 'observer'
     },
     {
         adjective: 'open-eyed',
@@ -65,14 +68,17 @@ const blobs = [
 const entries = Object.entries(blobs);
 let currentTimer;
 let lastStart;
-let speedSpan;
-let lastNoun, lastAdjective, adjective, noun;
+let speedSpan, changeCounter = 0;
+let lastNoun, lastAdjective, adjective, noun, body;
+let running = true;
+let remainsAfterPause;
 
 const init = () => {
     adjective = document.getElementById('adjective')
     noun = document.getElementById('noun')
-    generateContent(adjective, noun)
-    currentTimer = setInterval(() => generateContent(adjective, noun), speed);
+    body = document.getElementsByTagName('body')[0]
+    generateContent()
+    currentTimer = setInterval(() => generateContent(), speed);
     lastStart = Date.now();
     speedSpan = document.getElementById('speed')
     speedSpan.innerText = speed;
@@ -83,17 +89,31 @@ const memoizeLastContent = (adjective, noun) => {
     lastAdjective = adjective;
 }
 
-const generateContent = (adjective, noun) => {
+const generateContent = () => {
     if (counter < entries.length - 1) {
         const entry = entries[counter][1];
-        adjective.innerText = entry.adjective
-        noun.innerText = entry.noun
-        memoizeLastContent(entry.adjective)
-        memoizeLastContent(entry.noun)
+        changeContent(entry.noun, entry.adjective, entry.color)
     } else {
         randomize()
     }
     counter++;
+}
+
+const changeContent = (newNoun, newAdjective, color) => {
+    adjective.innerText = newAdjective
+    noun.innerText = newNoun
+    memoizeLastContent(newAdjective, newNoun)
+    const indexOfAdjective = Object.values(blobs).map(blob => blob.adjective).indexOf(newAdjective)
+    console.log(indexOfAdjective)
+    const randomValue = Math.round(Math.random() * 255 % 255).toString(16)
+    // body.style.backgroundColor = color ?? `#${randomValue}${randomValue}${randomValue}`;
+    const pattern = document.getElementById('pattern')
+    body.style.color = Number.parseInt(randomValue, 16) % 2 === 1 ? `var(--color-1)` : `var(--color-2)`;
+    changeCounter++;
+    setTimeout(() => document.getElementsByClassName('active')[0]?.classList.toggle('active'), speed * 1.5)
+    document.getElementById(`pattern-${indexOfAdjective + 1}`).classList.toggle('active')
+    // pattern.style.backgroundImage = `var(--pattern-${changeCounter % 8})`;
+    document.getElementById('changeCounter').innerText = changeCounter;
 }
 
 const randomizeContent = () => {
@@ -101,9 +121,7 @@ const randomizeContent = () => {
     const random2 = Math.floor(Math.random() * 10, 10) % entries.length;
     const entry = entries[random][1]
     const entry2 = entries[random2][1]
-    adjective.innerText = entry.adjective
-    noun.innerText = entry2.noun
-    memoizeLastContent(entry.adjective, entry2.noun)
+    changeContent(entry2.noun, entry.adjective, entry.color)
 }
 
 document.addEventListener('DOMContentLoaded', init)
@@ -123,16 +141,9 @@ document.addEventListener('keydown', (e) => {
 
 // could be smarter to resume current loop but easier it is to just 'hard' restart when speed changed
 const restartLoop = () => {
-    // const now = Date.now()
-    // const difference = (now - lastStart) % speed;
-    // const remaining = speed - difference;
-    // console.log(difference)
-    // console.log(remaining)
-    // setTimeout(() => {
-
-    // }, remaining);
     clearInterval(currentTimer);
-    currentTimer = setInterval(() => generateContent(adjective, noun), speed);
+    currentTimer = setInterval(() => generateContent(), speed);
+    lastStart = Date.now();
 }
 
 const slowDown = () => {
@@ -150,4 +161,19 @@ const speedUp = () => {
 const randomize = () => {
     randomizeContent()
     restartLoop()
+}
+
+// not sure about this
+const togglePause = () => {
+    const now = Date.now()
+    if (running) {
+        remainsAfterPause = (now - lastStart) % speed;
+        clearInterval(currentTimer)
+    } else {
+        currentTimer = setTimeout(() => {
+            generateContent();
+            restartLoop()
+        }, remainsAfterPause)
+    }
+    running = !running;
 }
